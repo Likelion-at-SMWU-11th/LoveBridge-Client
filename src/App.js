a7939a668bf6e0792b86f6a40a1303ba1a4e154a
solux <lee.yeji0327@gmail.com>
2023-08-05 12:47:28 +0900

    라우터 설치 및 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Logobar from './component/ui/Logobar';
 
 function App() {
   return (
-    <></>
+    <Routes>
+    </Routes>
   );
 }
 
