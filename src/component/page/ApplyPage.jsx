d6efbc0b4644f7880c9240e455309a391c512027
solux <lee.yeji0327@gmail.com>
2023-08-11 22:37:37 +0900

    [feat] PagePath 추가(#7)

--- src/component/page/ApplyPage.jsx
+++ src/component/page/ApplyPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
+import PagePath from '../ui/PagePath';
+
+const Wrapper = styled.div`
+  padding: 45px 136px 0px 123px;
+`
 
 function ApplyPage() {
   return (
-    <div>ApplyPage</div>
+    <Wrapper>
+      <PagePath pathname1='신청목록'/>
+    </Wrapper>
   )
 }
 
