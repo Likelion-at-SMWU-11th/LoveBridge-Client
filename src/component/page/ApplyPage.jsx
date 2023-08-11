9435827f6bddfcb2f9abc6a4fb692c162bb35f9f
solux <lee.yeji0327@gmail.com>
2023-08-11 22:53:34 +0900

    [add] ApplyPage 틀 구현(#7)

--- src/component/page/ApplyPage.jsx
+++ src/component/page/ApplyPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import PagePath from '../ui/PagePath';
+import Select from 'react-select';
+import Button from '../ui/Button';
 
 const Wrapper = styled.div`
   padding: 45px 136px 0px 123px;
@@ -9,7 +11,7 @@ const Title = styled.div`
   font-size: 40px;
   font-weight: 700;
   letter-spacing: -2px;
-  margin-top: 47px;
+  margin-top: 8vh;
 `
 const Info = styled.div`
   color: #222;
@@ -18,6 +20,7 @@ const Info = styled.div`
   etter-spacing: -0.8px;
   margin-top: 5px;
   margin-bottom: 30px;
+  letter-spacing: -0.8px;
 `
 const SelectBox = styled.div`
   border: 0.973px solid rgba(0, 0, 0, 0.20);
@@ -31,8 +34,59 @@ const PuppleTxt = styled.div`
   font-weight: 350;
   letter-spacing: -0.778px;
 `
+const SelectContainer = styled.div`
+  margin: 3vh 0 3vh 0;
+`
+const SelectLine = styled.div`
+  display: flex;
+  padding: 3vh 0 0 10vw;
+`
+const RegionSelect = styled(Select)`
+  width: 20vw;
+  margin-left: 25px;
+
+  .select-placeholder-text {
+    color: #4F4F4F;
+  }
+`
+const CategorySelect = styled(Select)`
+  width: 15vw;
+`
+const SortSelect = styled(Select)`
+  width: 8vw;
+`
+const Txt = styled.div`
+  color: #222;
+  font-size: 15.565px;  
+  font-weight: 600;
+  letter-spacing: -0.778px;
+  padding: 7.592px 69.07px 16.538px 0px;
+`
+const ButtonContainer = styled.div`
+  display: flex;
+  width: fit-content;
+  margin: auto;
+  >Button {
+    width: 12vw;
+    border-radius: 7.783px;
+  }
+`
+const StyledButton = styled.div`
+  >Button {
+    background: #6D6F82;
+    width: 12vw;
+    margin-right: 5px;
+    border-radius: 7.783px;
+  }
+`
+const Total = styled.div`
+  display: flex;
+  margin: 5vh 0 1vh 0;
+`
 
 function ApplyPage() {
+  const [ total, setTotal ] = useState(0);
+
   return (
     <Wrapper>
       <PagePath pathname1='신청목록'/>
@@ -40,7 +94,36 @@ function ApplyPage() {
       <Info>한 눈에 보고 , 클릭 한번으로 서비스를 신청할 수 있습니다.</Info>
       <SelectBox>
         <PuppleTxt>항목을 선택해주세요.</PuppleTxt>
+        <SelectContainer>
+          <SelectLine><Txt>지역</Txt> 
+          <RegionSelect className="react-select-container"
+                    placeholder={<div className="select-placeholder-text">선택</div>}
+                    components={{
+                        IndicatorSeparator: () => null
+                    }}/>
+          </SelectLine>
+          <SelectLine><Txt>카테고리</Txt> 
+          <CategorySelect className="react-select-container"
+                    placeholder=""
+                    components={{
+                        IndicatorSeparator: () => null
+                    }}/>
+          </SelectLine>
+          <SelectLine><Txt>검색정렬</Txt> 
+          <SortSelect className="react-select-container"
+                    placeholder=""
+                    components={{
+                        IndicatorSeparator: () => null,
+                        
+                    }}/>
+          </SelectLine>
+        </SelectContainer>
+        <ButtonContainer>
+          <StyledButton><Button className="reset" title="초기화" /></StyledButton>
+          <Button className="search" title="검색" />
+        </ButtonContainer>
       </SelectBox>
+      <Total>총 &nbsp;<PuppleTxt>{total}</PuppleTxt> &nbsp;건의 복지서비스가 있습니다.</Total>
     </Wrapper>
   )
 }
