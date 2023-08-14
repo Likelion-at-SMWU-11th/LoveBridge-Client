12df1a0de832729701c1006689330dc694ff46b1
MinJaeSon <mjsonsj@gmail.com>
2023-08-15 03:28:54 +0900

    [style] 배너 스타일 수정

--- src/component/ui/Banner.jsx
+++ src/component/ui/Banner.jsx
@@ -115,8 +115,8 @@ const Slide1 = styled.div`
     margin-right: 160px;
   }
   button {
-    width: 276px;
-    height: 60px;
+    width: 300px;
+    height: 64px;
     flex-shrink: 0;
     border-radius: 10px;
     background: #ad88eb;
@@ -125,9 +125,9 @@ const Slide1 = styled.div`
     border: none;
     font-family: Noto Sans;
     text-align: center;
-    font-size: 22px;
+    font-size: 24px;
     font-style: normal;
-    font-weight: 500;
+    font-weight: 550;
     line-height: normal;
     float: right;
     margin-left: 286px;
@@ -143,9 +143,7 @@ const Slide1 = styled.div`
   }
 `;
 
-const Container = styled.div`
-  background-image: url("../img/banner1_bg.svg");
-`;
+const Container = styled.div``;
 
 const Catchphrase = styled.h3`
   color: #fff;
@@ -154,12 +152,13 @@ const Catchphrase = styled.h3`
   text-align: right;
   font-size: 46px;
   font-style: normal;
-  font-weight: 500;
+  font-weight: 600;
   line-height: normal;
   width: 560px;
   float: right;
   margin-top: 140px;
   margin-bottom: 10px;
+  margin-left: 24px;
 `;
 
 const Description = styled.p`
