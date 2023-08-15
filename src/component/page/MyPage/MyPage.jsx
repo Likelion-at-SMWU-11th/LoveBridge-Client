37805ef833a62394e426ff0ddbc7733780ce275e
solux <lee.yeji0327@gmail.com>
2023-08-15 23:11:56 +0900

    [chore] 마이페이지 디자인 변경(#16)

--- src/component/page/MyPage/MyPage.jsx
+++ src/component/page/MyPage/MyPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
 import PagePath from '../../ui/PagePath';
-import uploadIcon from '../../img/mypage_upload.svg';
-import applyIcon from '../../img/mypage_apply.svg';
-import haertIcon from '../../img/mypage_heart.svg';
-import enter from '../../img/enter.svg';
+import mydocIcon from '../../img/mydocIcon.svg';
+import myapplyIcon from '../../img/myapplyIcon.svg';
+import mylikeIcon from '../../img/mylikeIcon.svg';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
   padding: 45px 136px 0px 123px;
@@ -16,75 +16,55 @@ const Title = styled.div`
   letter-spacing: -2px;
   margin-top: 50px;
 `
-const PuppleLine = styled.hr`
-  border: 0;
-  height: 3px;
-  background: #AD88EB;
-  margin-top: 30px;
-  border-radius: 50px;
+const Info = styled.div`
+  color: #222;
+  font-size: 16px;
+  font-weight: 350;
+  letter-spacing: -0.8px;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  letter-spacing: -0.8px;
 `
 const MenuBox = styled.div`
   display: flex;
-  padding: 30px 0px 30px 0px;
-  border-bottom: 0.8px solid #CCC;
-
-  .enter {
-    width: 20px;
-    padding-top: 5px;
-  }
+  justify-content: space-between;
 `
-const UploadLine = styled.div`
-  display: flex;
-  align-items: center;
+const MenuBtn = styled(motion.button)`
+  border-radius: 30px;
+  border: 1px solid #DBDBDB;
+  background: #FDFDFD;
+  width: 32%;
+  text-align: left;
+  padding: 4%;
   cursor: pointer;
-
-  > img {
-    width: 30px;
-  }
   &:hover {
-    .enter {
-      filter: invert(62%) sepia(72%) saturate(1784%) hue-rotate(213deg) brightness(97%) contrast(90%);
-    }
+    background: #F4F2FB;
+    border: 1px solid #AD88EB;
   }
 `
-const ApplyLine = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-
-  > img {
-    width: 30px;
-  }
-  &:hover {
-    .enter {
-      filter: invert(62%) sepia(72%) saturate(1784%) hue-rotate(213deg) brightness(97%) contrast(90%);
-    }
-  }
+const Icon = styled.img`
+  margin-lefth:
 `
-const HeartLine = styled.div`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-
-  > img {
-    width: 30px;
-  }
-  &:hover {
-    .enter {
-      filter: invert(62%) sepia(72%) saturate(1784%) hue-rotate(213deg) brightness(97%) contrast(90%);
-    }
-  }
+const MenuName = styled.div`
+  color: #585858;
+  font-size: 25px;
+  font-weight: 600;
+  letter-spacing: -1px;
+  margin-top: 4%;
 `
-const Menu = styled.div`
-  color: #333;
-  font-size: 20px;
+const MenuInfo = styled.div`
+  color: #8D8D8D;
+  font-size: 16px;
   font-weight: 400;
-  margin-left: 20px;
-  margin-right: 15px;
-  &:hover {
-    color: #AD88EB;
-  }
+  letter-spacing: -0.8px;
+  margin-top: 2%;
+  margin-bottom: 10%;
 `
+const hoverVariants = {
+  grow: {
+    scale: 1.05
+  },
+};
 
 function MyPage() {
   const navigate = useNavigate();
@@ -92,21 +72,17 @@ function MyPage() {
     <Wrapper>
       <PagePath pathname1='마이페이지'/>
       <Title>마이페이지</Title>
-      <PuppleLine />
+      <Info>복지서비스 신청에 필요한 서류를 관리하고,  내가 신청한 프로그램과 찜한 목록을 확인할 수 있습니다. </Info>
       <MenuBox>
-      <UploadLine onClick={() => navigate('/my/doc')}>
-        <img src={uploadIcon}/><Menu>내 서류 등록</Menu><img className="enter" src={enter}/>
-      </UploadLine>
-      </MenuBox>
-      <MenuBox>
-      <ApplyLine onClick={() => navigate('/my/apply')}>
-        <img src={applyIcon}/><Menu>내가 신청한 프로그램</Menu><img className="enter" src={enter}/>
-      </ApplyLine>
-      </MenuBox>
-      <MenuBox>
-      <HeartLine onClick={() => navigate('/my/like')}>
-        <img src={haertIcon}/><Menu>내가 찜한 목록</Menu><img className="enter" src={enter}/>
-      </HeartLine>
+        <MenuBtn whileHover={["grow"]} variants={hoverVariants} onClick={() => navigate('/my/doc')}>
+          <Icon src={mydocIcon}/><MenuName>내 서류 등록</MenuName><MenuInfo>신청 관련 서류를 <br/>등록하고 관리할 수 있어요.</MenuInfo>
+        </MenuBtn>
+        <MenuBtn whileHover={["grow"]} variants={hoverVariants} onClick={() => navigate('/my/apply')}>
+          <Icon src={myapplyIcon}/><MenuName>내가 신청한 프로그램</MenuName><MenuInfo>내가 신청한 프로그램의  심사 여부를 <br/>확인할 수 있어요.</MenuInfo>
+        </MenuBtn>
+        <MenuBtn whileHover={["grow"]} variants={hoverVariants} onClick={() => navigate('/my/like')}>
+          <Icon src={mylikeIcon}/><MenuName>내가 찜한 목록</MenuName><MenuInfo>내가 찜한 프로그램을 확인할 수 있어요.<br/>&nbsp;</MenuInfo>
+        </MenuBtn>
       </MenuBox>
     </Wrapper>
   )
