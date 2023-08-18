be83861f8750020d16a70ecc737430bf7959a19e
solux <lee.yeji0327@gmail.com>
2023-08-18 21:37:40 +0900

    [fix] react-dom/client 문제 해결

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import ScrollToTop from '../src/component/ui/ScrollToTop.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <ScrollToTop/>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
