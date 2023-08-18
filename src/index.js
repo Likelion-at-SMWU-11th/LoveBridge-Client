b2f171c4a1292256bd54508db6835a3f7702d8aa
solux <lee.yeji0327@gmail.com>
2023-08-18 18:07:20 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from '../src/component/ui/ScrollToTop.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
